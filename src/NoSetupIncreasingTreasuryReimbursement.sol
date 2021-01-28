2e1446ada0b8fd2571b423635662f08198216dbd
stefanionescu <stefanionescu@protonmail.com>
2021-01-28 00:00:17 +0000

    Add no setup increasing reimbursement contract

---
+++ src/NoSetupIncreasingTreasuryReimbursement.sol
@@ -0,0 +1,113 @@
+pragma solidity 0.6.7;
+
+import "./math/GebMath.sol";
+
+abstract contract StabilityFeeTreasuryLike {
+    function getAllowance(address) virtual external view returns (uint, uint);
+    function systemCoin() virtual external view returns (address);
+    function pullFunds(address, address, uint) virtual external;
+}
+
+contract NoSetupIncreasingTreasuryReimbursement is GebMath {
+    // --- Auth ---
+    mapping (address => uint) public authorizedAccounts;
+    function addAuthorization(address account) virtual external isAuthorized {
+        authorizedAccounts[account] = 1;
+        emit AddAuthorization(account);
+    }
+    function removeAuthorization(address account) virtual external isAuthorized {
+        authorizedAccounts[account] = 0;
+        emit RemoveAuthorization(account);
+    }
+    modifier isAuthorized {
+        require(authorizedAccounts[msg.sender] == 1, "NoSetupIncreasingTreasuryReimbursement/account-not-authorized");
+        _;
+    }
+
+    // --- Variables ---
+    // Starting reward for the fee receiver/keeper
+    uint256 public baseUpdateCallerReward;          // [wad]
+    // Max possible reward for the fee receiver/keeper
+    uint256 public maxUpdateCallerReward;           // [wad]
+    // Max delay taken into consideration when calculating the adjusted reward
+    uint256 public maxRewardIncreaseDelay;          // [seconds]
+    // Rate applied to baseUpdateCallerReward every extra second passed beyond a certain point (e.g next time when a specific function needs to be called)
+    uint256 public perSecondCallerRewardIncrease;   // [ray]
+
+    // SF treasury
+    StabilityFeeTreasuryLike  public treasury;
+
+    // --- Events ---
+    event AddAuthorization(address account);
+    event RemoveAuthorization(address account);
+    event ModifyParameters(
+      bytes32 parameter,
+      address addr
+    );
+    event ModifyParameters(
+      bytes32 parameter,
+      uint256 val
+    );
+    event FailRewardCaller(bytes revertReason, address feeReceiver, uint256 amount);
+
+    constructor() public {
+        authorizedAccounts[msg.sender] = 1;
+        maxRewardIncreaseDelay         = uint(-1);
+
+        emit AddAuthorization(msg.sender);
+    }
+
+    // --- Boolean Logic ---
+    function either(bool x, bool y) internal pure returns (bool z) {
+        assembly{ z := or(x, y)}
+    }
+    function both(bool x, bool y) internal pure returns (bool z) {
+        assembly{ z := and(x, y)}
+    }
+
+    // --- Treasury ---
+    /**
+    * @notice This returns the stability fee treasury allowance for this contract by taking the minimum between the per block and the total allowances
+    **/
+    function treasuryAllowance() public view returns (uint256) {
+        (uint total, uint perBlock) = treasury.getAllowance(address(this));
+        return minimum(total, perBlock);
+    }
+    /*
+    * @notice Get the SF reward that can be sent to a function caller right now
+    */
+    function getCallerReward(uint256 timeOfLastUpdate, uint256 defaultDelayBetweenCalls) public view returns (uint256) {
+        if (either(timeOfLastUpdate >= now, both(baseUpdateCallerReward == 0, maxUpdateCallerReward == 0))) return 0;
+        uint256 timeElapsed = (timeOfLastUpdate == 0) ? defaultDelayBetweenCalls : subtract(now, timeOfLastUpdate);
+        if (either(timeElapsed < defaultDelayBetweenCalls, baseUpdateCallerReward == 0)) {
+            return 0;
+        }
+        uint256 adjustedTime      = subtract(timeElapsed, defaultDelayBetweenCalls);
+        uint256 maxPossibleReward = minimum(maxUpdateCallerReward, treasuryAllowance() / RAY);
+        if (adjustedTime > maxRewardIncreaseDelay) {
+            return maxPossibleReward;
+        }
+        uint256 calculatedReward = baseUpdateCallerReward;
+        if (adjustedTime > 0) {
+            calculatedReward = rmultiply(rpower(perSecondCallerRewardIncrease, adjustedTime, RAY), calculatedReward);
+        }
+        if (calculatedReward > maxPossibleReward) {
+            calculatedReward = maxPossibleReward;
+        }
+        return calculatedReward;
+    }
+    /**
+    * @notice Send a stability fee reward to an address
+    * @param proposedFeeReceiver The SF receiver
+    * @param reward The system coin amount to send
+    **/
+    function rewardCaller(address proposedFeeReceiver, uint256 reward) internal {
+        if (address(treasury) == proposedFeeReceiver) return;
+        if (either(address(treasury) == address(0), reward == 0)) return;
+        address finalFeeReceiver = (proposedFeeReceiver == address(0)) ? msg.sender : proposedFeeReceiver;
+        try treasury.pullFunds(finalFeeReceiver, treasury.systemCoin(), reward) {}
+        catch(bytes memory revertReason) {
+            emit FailRewardCaller(revertReason, finalFeeReceiver, reward);
+        }
+    }
+}
